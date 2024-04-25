a8488e95baa6774600fb2472f12e679088b46b0b
uzziel <aliafifmm@gmail.com>
2024-04-25 22:14:25 +0330

    fix initial script not running correctly

--- elasticsearch/elastic_initial_setup_script.sh
+++ elasticsearch/elastic_initial_setup_script.sh
@@ -31,75 +31,78 @@ function create_common() {
     curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:{{ env "ELASTIC PASSWORD" }}" -H "Content-Type: application/json" https://es01:9200/_security/user/apm_system/_password -d '{"password":"{{ env "APM_SYSTEM_PASSWORD" }}"}'
     curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:{{ env "ELASTIC PASSWORD" }}" -H "Content-Type: application/json" https://es01:9200/_security/user/beats_system/_password -d '{"password":"{{ env "BEATS_SYSTEM_PASSWORD" }}"}'
     curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:{{ env "ELASTIC PASSWORD" }}" -H "Content-Type: application/json" https://es01:9200/_security/user/remote_monitoringuser/_password -d '{"password":"{{ env "REMOTE_MONITORING_USER_PASSWORD" }}"}'
+
+    # Create logstash_writer role and user
+    f; curl -s -XPUT --cacert config/certs/ca/ca.crt -u "elastic:{{ env "ELASTIC_PASSWORD" }}" "https://es01:9200/_security/role/{{ env "LOGSTASH_WRITER_USER" }}" -H 'Content-Type: application/json' -d'
+    { 
+        "cluster" : [
+            "monitor",
+            "read_ilm",
+            "manage",
+            "manage_pipeline",
+            "manage_ingest_pipelines",
+            "manage_index_templates",
+            "manage_ilm"
+        ],
+        "indices" : [
+            {
+                "names": [ "*" ],
+                "privileges" : [
+                    "create",
+                    "create_doc",
+                    "create_index",
+                    "write",
+                    "index",
+                    "view_index_metadata",
+                    "auto_configure",
+                    "manage",
+                    "read",
+                    "manage_ilm"
+                ],
+                "allow_restricted_indices": false
+            }
+        ]
+    }'
+
+    f; curl -s -XPOST --cacert config/certs/ca/ca.crt -u "elastic:{{ env "ELASTIC_PASSWORD" }}" "https://es01:9200/_security/user/{{ env "LOGSTASH_WRITER_USER" }}" -H 'Content-Type: application/json' -d'
+    { 
+        "roles" : [
+            "{{ env "LOGSTASH_WRITER_USER" }}"
+        ],
+        "password" : "{{ env "LOGSTASH_WRITER_PASSWORD" }}"
+    }'
+
+    f; curl -s -XPOST --cacert config/certs/ca/ca.crt -u "elastic:{{ env "ELASTIC_PASSWORD" }}" "https://es01:9200/_snapshot/snapshot-1" -H 'Content-Type: application/json' -d'
+    {
+        "type" : "fs",
+        "settings" : {
+            "compress" : "true",
+            "location" : "/app/elasticsearch/snapshot"
+        }
+    }'
+
+
+    ##########
+    # spaces #
+    ##########
+    f; curl -s -XPOST --cacert config/certs/ca/ca.crt -u "elastic:{{ env "ELASTIC_PASSWORD" }}" "https://kibana:5601/api/spaces/space" -H 'Content-Type: application/json' -H 'kbn-xsrf: reporting' -d'
+    {
+        "color": "#860000",
+        "description": "CustomSpace",
+        "disabledFeatures": [
+            "graph",
+            "monitoring",
+            "m1",
+            "maps",
+            "canvas",
+            "siem"
+        ],
+        "id": "customspace",
+        "imageUrl": "",
+        "initials": "C",
+        "name": "CustomSpace"
+    }'
+
 }
 
-# Create logstash_writer role and user
-f; curl -s -XPUT --cacert config/certs/ca/ca.crt -u "elastic:{{ env "ELASTIC_PASSWORD" }}" "https://es01:9200/_security/role/{{ env "LOGSTASH_WRITER_USER" }}" -H 'Content-Type: application/json' -d'
-{ 
-    "cluster" : [
-        "monitor",
-        "read_ilm",
-        "manage",
-        "manage_pipeline",
-        "manage_ingest_pipelines",
-        "manage_index_templates",
-        "manage_ilm"
-    ],
-    "indices" : [
-        {
-            "names": [ "*" ],
-            "privileges" : [
-                "create",
-                "create_doc",
-                "create_index",
-                "write",
-                "index",
-                "view_index_metadata",
-                "auto_configure",
-                "manage",
-                "read",
-                "manage_ilm"
-            ],
-            "allow_restricted_indices": false
-        }
-    ]
-}'
-
-f; curl -s -XPOST --cacert config/certs/ca/ca.crt -u "elastic:{{ env "ELASTIC_PASSWORD" }}" "https://es01:9200/_security/user/{{ env "LOGSTASH_WRITER_USER" }}" -H 'Content-Type: application/json' -d'
-{ 
-    "roles" : [
-        "{{ env "LOGSTASH_WRITER_USER" }}"
-    ],
-    "password" : "{{ env "LOGSTASH_WRITER_PASSWORD" }}"
-}'
-
-f; curl -s -XPOST --cacert config/certs/ca/ca.crt -u "elastic:{{ env "ELASTIC_PASSWORD" }}" "https://es01:9200/_snapshot/snapshot-1" -H 'Content-Type: application/json' -d'
-{
-    "type" : "fs",
-    "settings" : {
-        "compress" : "true",
-        "location" : "/app/elasticsearch/snapshot"
-    }
-}'
-
-
-##########
-# spaces #
-##########
-f; curl -s -XPOST --cacert config/certs/ca/ca.crt -u "elastic:{{ env "ELASTIC_PASSWORD" }}" "https://kibana:5601/api/spaces/space" -H 'Content-Type: application/json' -H 'kbn-xsrf: reporting' -d'
-{
-    "color": "#860000",
-    "description": "CustomSpace",
-    "disabledFeatures": [
-        "graph",
-        "monitoring",
-        "m1",
-        "maps",
-        "canvas",
-        "siem"
-    ],
-    "id": "customspace",
-    "imageUrl": "",
-    "initials": "C",
-    "name": "CustomSpace"
-}'
+create_common;
