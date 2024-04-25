75b540c2162d9c237f7d7cfc19bf2afd4c1a1bf7
uzziel <aliafifmm@gmail.com>
2024-04-25 19:42:06 +0330

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -56,8 +56,8 @@ chmod 666 "${ELK_DIR}"/logstash/logstash.yml; \
 chmod 744 "${ELK_DIR}"/elasticsearch/elastic_initial_setup_script.sh; \
 find "${DATA_DIR}"/elasticsearch/data/ -type d | xargs chmod 775; \
 find "${DATA_DIR}"/elasticsearch/data/ -type f | xargs chmod 664; \
-find "${ELK_DIR}" -type d | xargs chmod 775; \
-find "${ELK_DIR}" -type f | xargs chmod 664;
+find "${ELK_DIR}" -type d | xargs -L1 chmod 775; \
+find "${ELK_DIR}" -type f | xargs -L1 chmod 664;
 
 # Tune kernel
 grep -q 'vm.max_map_count=262144' /etc/sysctl.conf || echo 'vm.max_map_count=262144' >> /etc/sysctl.conf && sysctl --load /etc/sysctl.conf;
